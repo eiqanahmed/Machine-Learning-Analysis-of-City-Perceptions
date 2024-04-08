4dc6b00ec39310c0a7bcd1318ca6f7260e32494c
Eiqan <117050195+eiqanahmed@users.noreply.github.com>
2024-04-08 13:27:10 -0400

    Update pred.py, added embeddings

--- pred.py
+++ pred.py
@@ -213,17 +213,243 @@ def clean_up_input(data):
     return data
 
 
+import numpy as np
+import pandas as pd
+import sys
+import csv
+import re
+import random
+
+
+def dist_single(v, x):
+    """
+    Compute the squared Euclidean distance between vectors `v` and `x`.
+
+    Parameters:
+        `v` - a numpy array (vector) representing one student's answers, shape (17,)
+        `x` - a numpy array (vector) representing another student's answers, shape (17,)
+
+    Returns: a scalar value representing their squared Euclidean distance
+    """
+
+    diff = x - v # compute a difference vector  (x-v)
+
+    sqdiff = diff**2
+
+    sumval = np.sum(sqdiff)
+
+    return sumval
+
+
+def predict(v, X_train, t_train, k=1):
+    """
+    Returns a prediction using the k-NN
+
+    Parameters:
+        `v` - a numpy array (vector) representing a student's answers, shape (17,)
+        `X_train` - a data matrix representing a set of answers from students, shape (N, 17)
+        `t_train` - a vector of ground-truth labels, shape (N,)
+        `k` - a positive integer 1 < k <= N, describing the number of closest set of answers
+              to consider as part of the knn algorithm
+
+    Returns:
+        A prediction for the city the student was considering while answering the questions.
+    """
+    dists = dist_all(v, X_train)
+
+    enum_list = list(enumerate(dists))
+    sorted_enum_list = sorted(enum_list, key=lambda x: x[1])
+
+    sorted_dists = [value for index, value in sorted_enum_list]
+    sorted_indices = [index for index, value in sorted_enum_list]
+    indices = []
+    j = 0
+    while j < k:
+      indices.append(sorted_indices[j])
+      j += 1
+
+    ts = t_train[np.array(indices)]
+
+    unique_elements, counts = np.unique(ts, return_counts=True)
+    most_common_index = np.argmax(counts)
+    most_common = unique_elements[most_common_index]
+
+    prediction = most_common
+
+    return prediction
+
+
+def compute_accuracy(X_new, t_new, X_train, t_train, k=1):
+    num_predictions = 0
+    num_correct = 0
+
+    for i in range(X_new.shape[0]): # iterate over each set of answers index in X_new
+        v = X_new[i] # one set of answers from a student
+        t = t_new[i] # prediction target
+
+        prediction = predict(v, X_train, t_train, k=k)
+
+        if prediction == t:
+          num_correct += 1
+
+        num_predictions += 1
+
+    return num_correct / num_predictions
+
+
+def normalize(X, continuous_indices):
+    X_cpy = X.copy()
+
+    # Normalize each continuous feature
+    for index in continuous_indices:
+
+        mean = np.mean(X_cpy[:, index])
+        std = np.std(X_cpy[:, index])
+
+        if std > 0:
+            X_cpy[:, index] = (X_cpy[:, index] - mean) / std
+        else:
+            # If standard deviation is zero, leave the values as they are
+            pass
+
+    return X_cpy
+
+
+def dist_all(v, X):
+    """
+    Compute the squared Euclidean distance between one data point `v` (vector) and the
+    data points in the data matrix `X`.
+
+    Parameters:
+        `v` - a numpy array (vector) representing one students answers to the first 9 questions shape (17,)
+        `X` - a data matrix representing a set of answers from all students
+        who participated in the survey, shape (N, 17)
+
+    Returns: a vector of squared Euclidean distances between `v` and each image in `X`,
+             shape (N,)
+    """
+
+    diff = X - v
+
+    sqdiff = diff**2
+
+    sumval = np.sum(sqdiff, axis=1)
+
+    return sumval
+
+
+def to_numeric(s):
+    """Converts string `s` to a float.
+
+    Invalid strings and NaN values will be converted to float("nan").
+    """
+
+    if isinstance(s, str):
+        s = s.replace(",", "")
+        s = pd.to_numeric(s, errors="coerce")
+    return float(s)
+
+
+def get_number_list(s):
+    """Get a list of integers contained in string `s`
+    """
+    return [int(n) for n in re.findall("(\d+)", str(s))]
+
+def get_number_list_clean(s):
+    """Return a clean list of numbers contained in `s`.
+
+    Additional cleaning includes removing numbers that are not of interest
+    and standardizing return list size.
+    """
+
+    n_list = get_number_list(s)
+    n_list += [-1]*(6-len(n_list))
+    return n_list
+
+
+def get_number(s):
+    """Get the first number contained in string `s`.
+
+    If `s` does not contain any numbers, return -1.
+    """
+    n_list = get_number_list(s)
+    return n_list[0] if len(n_list) >= 1 else -1
+
+
+def find_area_at_rank(l, i):
+    """Return the area at a certain rank in list `l`.
+
+    Areas are indexed starting at 1 as ordered in the survey.
+
+    If area is not present in `l`, return -1.
+    """
+    return l.index(i) + 1 if i in l else -1
+
+
+def cat_in_s(s, cat):
+    """Return if a category is present in string `s` as an binary integer.
+    """
+    return int(cat in s) if not pd.isna(s) else 0
+
+
+# Vectorization
+
+def process_Q56(data):
+    categories = ["Friends", "Co-worker", "Siblings", "Partner"]
+    for category in categories:
+        data[f"Q5_{category}"] = (data["Q5"] == category).astype(int)
+
+    categories_q5 = ["Friends", "Co-worker", "Sibling", "Partner"]
+    categories_q6 = ["Skyscrapers", "Sport", "Art and Music", "Carnival", "Cuisine", "Economic"]
+    patterns = {category: f"{category}=>(\d+)" for category in categories_q6}
+
+    for category in categories_q5:
+        data[f"Q5_{category}"] = data["Q5"].str.contains(category, na=False).astype(int)
+
+    del data["Q5"]
+
+    for category, pattern in patterns.items():
+        data[category] = data["Q6"].str.extract(pattern).astype(float).fillna(0)
+
+    del data["Q6"]
+
+
+def clean_up_input(data):
+    data["Q7"] = data["Q7"].apply(to_numeric).fillna(0)
+    data["Q8"] = data["Q8"].apply(to_numeric).fillna(0)
+    data["Q9"] = data["Q9"].apply(to_numeric).fillna(0)
+
+    # Clean for number categories
+    data["Q1"] = data["Q1"].apply(get_number)
+    data["Q2"] = data["Q2"].apply(get_number)
+    data["Q3"] = data["Q3"].apply(get_number)
+    data["Q4"] = data["Q4"].apply(get_number)
+
+    return data
+
+
 def predict_all(file_path):
-
+    file_path = "/content/clean_dataset.csv"
+    
     # get the file path create a function for it
-
-    file_path = "/Users/eiqan/PycharmProjects/Data_Vectorization/clean_dataset.csv"
 
     data = pd.read_csv(file_path)
 
     process_Q56(data)
     data = clean_up_input(data)
 
+    documents = [TaggedDocument(words=doc.split(), tags=[i]) for i, doc in enumerate(data['Q10'].fillna(""))]
+
+# Train a Doc2Vec model
+    model = Doc2Vec(documents, vector_size=20, window=2, min_count=1, workers=4, epochs=40)
+
+    # Generate embeddings for each description in "Q10"
+    Q10_embeddings = np.array([model.dv[i] for i in range(len(documents))])
+    Q10_embeddings_df = pd.DataFrame(Q10_embeddings, columns=[f'Q10_embed_{i}' for i in range(Q10_embeddings.shape[1])])
+    # Assuming `data` is your existing DataFrame
+    data = pd.concat([data.reset_index(drop=True), Q10_embeddings_df.reset_index(drop=True)], axis=1)
+    # print(data.iloc[-1])
+    
     data_fets = np.stack([
         (data["Q1"]),
         (data["Q2"]),
@@ -242,8 +468,29 @@ def predict_all(file_path):
         (data["Q7"]),
         (data["Q8"]),
         (data["Q9"]),
+        data["Q10_embed_0"],
+        data["Q10_embed_1"],
+        data["Q10_embed_2"],
+        data["Q10_embed_3"],
+        data["Q10_embed_4"],
+        data["Q10_embed_5"],
+        data["Q10_embed_6"],
+        data["Q10_embed_7"],
+        data["Q10_embed_8"],
+        data["Q10_embed_9"],
+        data["Q10_embed_10"],
+        data["Q10_embed_11"],
+        data["Q10_embed_12"],
+        data["Q10_embed_13"],
+        data["Q10_embed_14"],
+        data["Q10_embed_15"],
+        data["Q10_embed_16"],
+        data["Q10_embed_17"],
+        data["Q10_embed_18"],
+        data["Q10_embed_19"],
     ], axis=1)
 
+    # Now explicitly check for any non-numeric types or NaN values in columns to be used
     if np.isnan(data_fets).any():
         print("NaN values detected in the dataset. Consider imputation.")
 
@@ -279,7 +526,7 @@ def predict_all(file_path):
     # print(X_test.shape)
     # print(D.shape) , should be (1468, 18)
 
-    continuous_indices = [0, 1, 2, 3, 14, 15, 16]
+    continuous_indices = [14, 15, 16]
     X_train_norm = normalize(X_train, continuous_indices)
     X_valid_norm = normalize(X_valid, continuous_indices)
     X_test_norm = normalize(X_test, continuous_indices)
@@ -298,8 +545,22 @@ def predict_all(file_path):
         predictions.append(pred)
 
     print(compute_accuracy(X_test_norm, t_test, X_train_norm, t_train, k))
+    # print(predictions)
 
     return predictions
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
